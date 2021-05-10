528ddd4af554a100585f497a5fd6b6bf6a81c3b9
= <=>
2021-05-10 03:41:25 -0300

    Created nav and menu, also started the image slider - added some style and js code

---
+++ scripts/hamburger-menu.js
@@ -0,0 +1,9 @@
+//This code is used to toggle the change class to display our hamburger menu
+
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("change");
+
+    document.getElementById("nav").classList.toggle("change");
+
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
